bc79ac786af9d98f7f562e6a5b712c4250832a38
Adam Haile <adammhaile@gmail.com>
2017-05-31 13:18:36 -0400

    new error if installing on Py3

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup
+import sys
+from sys import version_info as vi
+
+if vi.major != 2:
+    ver = '{}.{}.{}'.format(vi.major, vi.minor, vi.micro)
+    error = (
+        'INSTALLATION ERROR!\n'
+        'BiblioPixel v2 requires Python 2.7 and you are using {}!\n'
+        'This is an older BiblioPixel and if that was intentional '
+        'you will have to install it under Python 2.7\n'
+        'However, we highly recommend using the latest BiblioPixel '
+        '(v3+) with Python 3.4+\n'
+        '> python3 -m pip install bibliopixel'
+        '\n'
+    )
+    print(error.format(ver))
+    sys.exit(1)
+
 import bibliopixel
 
 setup(
